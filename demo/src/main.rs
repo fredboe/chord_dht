506de99527b153c05bd68c84163fdfccec1cb803
fredboe <fredboe@outlook.com>
2023-10-24 23:10:57 +0200

    Execution of hostname -I to access the ip address

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,12 +1,16 @@
+use anyhow::{anyhow, Result};
 use chord::dht_demo::SimpleChordDHT;
 use std::error::Error;
+use std::net::IpAddr;
+use std::process::Command;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     println!("Joining the network.");
 
-    let own_ip = std::env::var("OWN_IP").unwrap();
+    let own_ip = retrieve_own_ip()?;
     println!("own ip: {}", own_ip);
-    let dht_handle = SimpleChordDHT::new_network(own_ip.parse().unwrap()).await?;
+    let dht_handle = SimpleChordDHT::new_network(own_ip).await?;
 
     dht_handle.put("A".to_string(), "a".to_string()).await?;
     dht_handle.put("B".to_string(), "b".to_string()).await?;
@@ -26,7 +30,20 @@ async fn main() -> Result<(), Box<dyn Error>> {
     println!("The value was: {:?}.", value);
 
     println!("Leaving the network.");
-    //dht_handle2.leave().await?;
     dht_handle.leave().await?;
     Ok(())
+}
+
+fn retrieve_own_ip() -> Result<IpAddr> {
+    let output = Command::new("hostname").arg("-I").output()?.stdout;
+    let output_string = String::from_utf8_lossy(&output);
+
+    let first_ip_str = output_string
+        .trim()
+        .split_whitespace()
+        .next()
+        .ok_or(anyhow!("There was no ip available."))?;
+    let ip = first_ip_str.parse()?;
+
+    Ok(ip)
 }
